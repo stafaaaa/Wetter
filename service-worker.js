c0e11321ce8ad513f85d56169393bd9a8e9bcf6b
stafaaaa <mosgab69@gmail.com>
2026-04-12 12:09:58 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "dashboard-cache-v2";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./dashboard_3d_sw_fixed.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./screenshot1.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME ? caches.delete(key) : Promise.resolve())
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
